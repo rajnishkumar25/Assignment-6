b620312facdb479bd4b52df191bb366e08ba3f66
rajnishkumar25 <94224509+rajnishkumar25@users.noreply.github.com>
2021-11-20 02:38:31 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class q1 
+
+{
+public static void main(String[] args) 
+
+{
+    Scanner sc=new Scanner (System.in);
+    
+   System.out.println(" Enter the variable");
+
+   int a= sc.nextInt();
+
+   switch(a)
+
+   { case 1 :
+    System.out.println("good");
+    break;
+
+    case 2 :
+    System.out.println("better");
+    break;
+
+    case 3:
+    System.out.println("best");
+    break;
+
+    default:
+    System.out.println("invalid");
+}
+
+   
+
+   
+    
+    
+}
+
+}
---
+++ q2.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class q2 
+
+{
+public static void main(String[] args) 
+
+{
+//     Scanner sc=new Scanner (System.in);
+    
+//    System.out.println(" Enter the variable");
+
+//    int a= sc.nextInt();
+
+//    switch(a)
+
+//    { case 1 :
+//     System.out.println("good");
+//     break;
+
+//     case 2 :
+//     System.out.println("better");
+//     break;
+
+//     case 3:
+//     System.out.println("best");
+//     break;
+
+//     default:
+//     System.out.println("invalid");
+
+int ch=2;
+switch(ch) {
+case 1: System.out.println("All courses");
+break;
+case 2: System.out.println("I love Java!");
+case 3: System.out.println("I love Python");
+}
+
+}
+
+}
